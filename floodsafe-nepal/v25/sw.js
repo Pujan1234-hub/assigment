344dcc43c96c1e19046c248f27e8bfc566ddd22c
Pujan1234-hub <chapagainpujan058@gmail.com>
2026-08-27 22:36:47 +0100

    Refresh V25 live core cache

--- floodsafe-nepal/v25/sw.js
+++ floodsafe-nepal/v25/sw.js
@@ -1,4 +1,4 @@
-const CACHE='floodsafe-nepal-v25-shell-7';
+const CACHE='floodsafe-nepal-v25-shell-8';
 const SHELL=['./','./index.html','./map.html','./alerts.html','./people.html','./weather.html','./news.html','./sources.html','./shell.css','./live-core.css','./live-core.js','./app.js','./ui-cleanup.js','./national-news.js','./manifest.webmanifest','../v24/icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('floodsafe-nepal-v25-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));

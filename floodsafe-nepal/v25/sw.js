c8054e347001112417331609e5c8506dbc58e637
Pujan1234-hub <chapagainpujan058@gmail.com>
2026-08-27 21:52:33 +0100

    Ship simplified V25 UI copy

--- floodsafe-nepal/v25/sw.js
+++ floodsafe-nepal/v25/sw.js
@@ -1,5 +1,5 @@
-const CACHE='floodsafe-nepal-v25-shell-2';
-const SHELL=['./','./index.html','./alerts.html','./people.html','./weather.html','./news.html','./sources.html','./shell.css','./app.js','./manifest.webmanifest','../v24/icon.svg'];
+const CACHE='floodsafe-nepal-v25-shell-3';
+const SHELL=['./','./index.html','./alerts.html','./people.html','./weather.html','./news.html','./sources.html','./shell.css','./app.js','./ui-cleanup.js','./manifest.webmanifest','../v24/icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('floodsafe-nepal-v25-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{

9008ed894c053f835244e693ed89fcdbcd936d0c
Pujan1234-hub <chapagainpujan058@gmail.com>
2026-08-31 16:28:27 +0100

    Bump current map engine cache version

--- floodsafe-nepal/v25/map-loader-v3.js
+++ floodsafe-nepal/v25/map-loader-v3.js
@@ -4,12 +4,12 @@ window.maplibregl = maplibregl;
 
 if(!window.__fsStableMapLoading){
   window.__fsStableMapLoading=true;
-  import('./map-stable-v2.js?v=4').then(()=>{
+  import('./map-stable-v2.js?v=5').then(()=>{
     window.__fsStableMapLoaded=true;
     return import('./hydro-complete-v1.js?v=4');
   }).then(()=>{
     window.__fsHydroCompleteLoaded=true;
-    return import('./map-runtime-fix-v1.js?v=1');
+    return import('./map-runtime-fix-v1.js?v=2');
   }).then(()=>{
     window.__fsMapRuntimeGuardLoaded=true;
   }).catch((err)=>{

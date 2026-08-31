280bda514a642c6b884b81ecb24e96f483d43a9f
Pujan1234-hub <chapagainpujan058@gmail.com>
2026-08-31 18:10:19 +0100

    Load current hydro map runtime version

--- floodsafe-nepal/v25/map-loader-v3.js
+++ floodsafe-nepal/v25/map-loader-v3.js
@@ -6,7 +6,7 @@ if(!window.__fsStableMapLoading){
   window.__fsStableMapLoading=true;
   import('./map-stable-v2.js?v=5').then(()=>{
     window.__fsStableMapLoaded=true;
-    return import('./hydro-complete-v1.js?v=4');
+    return import('./hydro-complete-v1.js?v=6');
   }).then(()=>{
     window.__fsHydroCompleteLoaded=true;
     return import('./map-runtime-fix-v1.js?v=2');

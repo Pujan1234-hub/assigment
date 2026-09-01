bf843f4f1a1ccfb3de269f3a1079721f6f28a4a3
Pujan1234-hub <chapagainpujan058@gmail.com>
2026-09-01 10:50:30 +0100

    Align river status UI with 10-minute freshness

--- floodsafe-nepal/v25/river-status-ui.js
+++ floodsafe-nepal/v25/river-status-ui.js
@@ -1,7 +1,7 @@
 (()=>{'use strict';
-if(window.__fsRiverStatusUIV3)return;window.__fsRiverStatusUIV3=true;
-const MAX=5*60*1000;
-function refresh(){window.FloodSafeMap?.refreshGauges?.();window.FloodSafeRiverCurrent5?.apply?.()}
+if(window.__fsRiverStatusUIV4)return;window.__fsRiverStatusUIV4=true;
+const MAX=10*60*1000;
+function refresh(){window.FloodSafeMap?.refreshGauges?.();window.FloodSafeRiverLatest?.apply?.()}
 for(const ev of['fstrustedriverupdate','fsriverupdate','fslanguage','online'])window.addEventListener(ev,()=>requestAnimationFrame(refresh));
 window.FloodSafeRiverStatusUI={refresh,MAX_AGE_MS:MAX,get catalogCount(){return window.FloodSafeMap?.stationCatalogCount||0},get currentCount(){return window.FloodSafeMap?.currentStationCount||0}};
 })();

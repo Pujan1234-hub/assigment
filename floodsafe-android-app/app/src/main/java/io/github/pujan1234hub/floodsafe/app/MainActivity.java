574e78b5f97aa779d46d43ca73e416d8f7f328d6
Pujan1234-hub <chapagainpujan058@gmail.com>
2026-09-03 01:51:23 +0100

    Fix notification update compile error

--- floodsafe-android-app/app/src/main/java/io/github/pujan1234hub/floodsafe/app/MainActivity.java
+++ floodsafe-android-app/app/src/main/java/io/github/pujan1234hub/floodsafe/app/MainActivity.java
@@ -209,6 +209,7 @@ public class MainActivity extends Activity {
     }
 
 
+    private void refreshConnection() {
         runOnUiThread(() -> { if (!isFinishing() && !isDestroyed()) updateConnection(); });
     }
 

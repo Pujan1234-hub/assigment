12a6cb70d1140235a973caa3386767728de186b5
Pujan1234-hub <chapagainpujan058@gmail.com>
2026-09-08 14:20:33 +0100

    Keep Android network callback cleanup compatible

--- floodsafe-android-app/app/src/main/java/io/github/pujan1234hub/floodsafe/app/MainActivity.java
+++ floodsafe-android-app/app/src/main/java/io/github/pujan1234hub/floodsafe/app/MainActivity.java
@@ -401,7 +401,7 @@ public class MainActivity extends Activity {
     }
     @Override protected void onDestroy() {
         finishLocation(false);
-        if (connectivity != null && networkCallback != null) connectivity.unregisterDefaultNetworkCallback(networkCallback);
+        if (connectivity != null && networkCallback != null) connectivity.unregisterNetworkCallback(networkCallback);
         if (webView != null) {
             ((android.view.ViewGroup) webView.getParent()).removeView(webView);
             webView.destroy();
